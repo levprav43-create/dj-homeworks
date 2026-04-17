a5a742e61a3fbe6cdb23cad33818ed0ab657e052
Lev Terekhin <levprav43@gmail.com>
2026-04-17 16:23:47 +0400

    ДЗ 2.2: реализовал связь многие-ко-многим статьи-теги через Scope

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormSet(BaseInlineFormSet):
+    """Формсет для проверки что есть один и только один основной тег"""
+    
+    def clean(self):
+        super().clean()
+        
+        # Считаем сколько основных тегов
+        main_count = 0
+        for form in self.forms:
+            if form.cleaned_data and form.cleaned_data.get('is_main'):
+                main_count += 1
+        
+        # Проверяем что есть ровно один основной тег
+        if main_count == 0:
+            raise ValidationError('Укажите хотя бы один основной тег!')
+        if main_count > 1:
+            raise ValidationError('Основной тег должен быть только один!')
+        
+        return self.cleaned_data
+
+
+class ScopeInline(admin.TabularInline):
+    """Inline для связи статья-тег"""
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormSet
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    """Админка для статей"""
+    inlines = [ScopeInline]
+    list_display = ['title', 'published_at']
+    list_filter = ['tags']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Админка для тегов"""
+    list_display = ['name']
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-
 from articles.models import Article
 
 
 def articles_list(request):
+    """Отображает список статей с тегами"""
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
+    
+    # Получаем параметр сортировки из GET-запроса (по умолчанию - новые сначала)
+    ordering = request.GET.get('order_by', '-published_at')
+    
+    # Получаем статьи с предзагрузкой тегов (оптимизация запросов)
+    articles = Article.objects.prefetch_related('scopes__tag').order_by(ordering)
+    
+    context = {
+        'object_list': articles,
+        'current_order': ordering
+    }
+    
     return render(request, template, context)
